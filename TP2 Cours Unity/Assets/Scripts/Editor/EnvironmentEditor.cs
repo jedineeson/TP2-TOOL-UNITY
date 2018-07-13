ca56f87ea3646c35774a616ac26d05efb8a86c3f
jedineeson <1810027@bart.ca>
2018-07-13 16:02:34 -0400

    tool begin

---
+++ TP2 Cours Unity/Assets/Scripts/Editor/EnvironmentEditor.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentEditor : MonoBehaviour 
+{
+	// Pour Afficher l'inspecteur par defaut sauf certains parametres, veuillez utiliser DrawPropertiesExcluding(serializedObject, "m_MyVariable");
+}
---
+++ TP2 Cours Unity/Assets/Scripts/Editor/ObjectCreator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ObjectCreator : EditorWindow
+{
+	private Transform m_Transform;
+	private Vector3 m_StartPos = new Vector3(0f, 0f, 0f);
+
+	private enum Direction
+	{
+		Up,
+		Down,
+		Left,
+		Right,
+		Forward,
+		Back
+	}
+
+	[MenuItem("Tool/Object Creator")]
+	private static void Init()
+	{
+		GetWindow<ObjectCreator>().Show();
+	}
+
+
+
+	private void OnGUI()
+	{
+		EditorGUILayout.BeginVertical(GUI.skin.box);
+		
+		EditorGUILayout.LabelField("Position", EditorStyles.centeredGreyMiniLabel);
+		
+		EditorGUILayout.BeginVertical(GUI.skin.box);
+		
+		EditorGUILayout.TextArea("If none is assigned, then the start position will be (0f, 0f, 0f)", EditorStyles.label);
+		
+		EditorGUILayout.EndVertical();
+		
+		m_Transform = EditorGUILayout.ObjectField("Start Position", m_Transform, typeof(Transform), false) as Transform;
+		
+		EditorGUILayout.EndVertical();
+		
+		EditorGUILayout.BeginVertical(GUI.skin.box);
+
+		EditorGUILayout.LabelField("Object Selection", EditorStyles.centeredGreyMiniLabel);
+		if(GUILayout.Button("Use Custom Object"))
+		{
+
+		}
+
+		EditorGUILayout.EndVertical();
+
+	}
+}
